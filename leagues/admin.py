57d06943632063118e01564245fb49e8e613df48
andrew-remon <andrewremon2005@outlook.com>
2026-06-24 21:21:06 +0300

    feat: polished admin interface and league report command

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,12 +1,17 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models import Count
 from .models import *
 
 # Register your models here.
+class TeamInline(admin.TabularInline):
+    model = Team
+    extra = 1
+
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
     list_display = ('name', "max_teams", 'created_at', 'get_team_count')
     search_fields = ('name',)
+    inlines=[TeamInline]
 
     # these two methods represent Separation of Concerns: Fetching and Displaying
 
@@ -20,21 +25,28 @@ class LeagueAdmin(admin.ModelAdmin):
     def get_team_count(self, obj):
         return obj.teams_count
 
-
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'league', 'city')
+    list_display = ('name', 'league', 'city', 'get_player_count', 'founded_year')
     list_select_related = ('league',)
-    list_filter = ('league',)
+    list_filter = ('league', 'city')
     search_fields = ('name', 'city', )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(player_count=Count("players"))
+
+    @admin.display(description="Player Count", ordering="player_count")
+    def get_player_count(self, obj):
+        return obj.player_count
 
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('get_full_name', 'team', 'position', 'jersey_number')
     list_select_related = ('team',)
-    list_filter = ('position', 'team')
-    search_fields = ('first_name', 'last_name', 'jersey_number')
+    list_filter = ('position', 'team__league')
+    search_fields = ('first_name', 'last_name',)
 
     @admin.display(description='Full Name', ordering='first_name')
     def get_full_name(self, obj):
@@ -44,14 +56,22 @@ class PlayerAdmin(admin.ModelAdmin):
 class MatchResultInline(admin.TabularInline):
     model = MatchResult
     extra = 0
+    max_num = 1
 
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     inlines = [MatchResultInline]
-    list_display = ('league', 'home_team', 'away_team', 'status', 'scheduled_date', 'get_match_result')
+    list_display = ('league', 'home_team', 'away_team', 'match_day', 'status', 'score_display')
+    actions=["mark_matches_as_complete"]
 
     @admin.display(description="Match Result")
-    def get_match_result(self,obj):
+    def score_display(self,obj):
         return obj.score_display
+
+    @admin.action(description="Mark selected matches as COMPLETED")
+    def mark_matches_as_complete(self, request, queryset):
+        matches_selected = queryset.update(status=Match.MatchStatus.COMPLETED)
+        self.message_user(request, f"{matches_selected} Matches selected is marked as completed", messages.INFO)
+
 
---
+++ leagues/management/commands/league_report.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand, CommandError
+from leagues.models import League, Match
+from leagues.services import get_standings
+from django.db.models import Count
+
+class Command(BaseCommand):
+    help = "Computes and prints the league info, current standing table and the upcoming matches."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'league_name',
+            type=str,
+            help="The League name"
+        )
+
+    def get_league(self, league_name):
+        try:
+            league = League.objects.get(name=league_name)
+        except League.DoesNotExist:
+            raise CommandError(f"League with ID {league_name} does not exist.")
+        return league
+
+    def print_league_info(self, league):
+        teams_count = league.teams.count()
+        matches_count = league.matches.count()
+
+        self.stdout.write(f"{'Name:':<4} {league.name}")
+        self.stdout.write(f"{'Teams Count:':<4} {teams_count}")
+        self.stdout.write(f"{'Matches Count:':<4} {matches_count}")
+
+        self.stdout.write("-" * 60)
+
+    def get_upcoming_matches(self, league):
+        upcoming_matches = league.matches.filter(status=Match.MatchStatus.SCHEDULED)
+
+        # Header Row
+        header = f"{'Pos':<4} {'Home Team':<20} {'Away Team':<20} {'Date':<10}"
+        self.stdout.write(header)
+        self.stdout.write("-" * 60)
+
+        # Data Rows
+        for pos, match in enumerate(upcoming_matches, 1):
+            row = (
+                f"{pos:<4} "
+                f"{str(match.home_team):<20} "
+                f"{str(match.away_team):<20} "
+                f"{str(match.scheduled_date):<10} "
+            )
+            self.stdout.write(row)
+
+        self.stdout.write("=" * 60 + "\n")
+
+    def handle(self, *args, **options):
+        league = self.get_league(options["league_name"])
+        self.print_league_info(league)
+        get_standings(league.id)
+        self.get_upcoming_matches(league)
+
+
